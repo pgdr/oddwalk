f3ccfdaf6685f00dce59f19bc538a15024e0c03c
Pål Grønås Drange <Pal.Drange@uib.no>
2025-02-22 00:31:22 +0100

    Initial commit

---
+++ oddwalk.py
@@ -0,0 +1,96 @@
+import sys
+import math
+import heapq
+from collections import defaultdict
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def dist(u, v, pos):
+    ux, uy = pos[u]
+    vx, vy = pos[v]
+    return round(math.hypot(ux - vx, uy - vy), 3)
+
+
+def plot_graph(G, path, pos):
+    plt.figure(figsize=(10, 8))
+
+    node_color = ["red" if node in path else "lightblue" for node in G.nodes()]
+
+    nx.draw(
+        G, pos, with_labels=True, node_color=node_color, node_size=500, font_size=10
+    )
+
+    edge_labels = {(u, v): f'{d["weight"]:.2f}' for u, v, d in G.edges(data=True)}
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+
+    plt.title(sys.argv[1])
+    plt.show()
+
+
+def cheapest_walk(G, s, k):
+    opt = defaultdict(lambda: 10**8)
+    PQ = []
+
+    # l, k, p, v
+    heapq.heappush(PQ, (0, 0, 0, s))
+
+    while PQ:
+        ell, k_prime, p, v = heapq.heappop(PQ)
+
+        if opt[(k_prime, p, v)] < ell:
+            continue  # discard sub-optimal vector
+
+        opt[(k_prime, p, v)] = ell
+
+        for u in G[v]:
+            w = G.edges[u, v]["weight"]
+            heapq.heappush(PQ, (round(ell + w, 2), k_prime, 1 - p, u))
+            if k_prime < k:
+                heapq.heappush(PQ, (ell, k_prime + 1, 1 - p, u))
+
+    return opt
+
+
+def read_graph(fname):
+    edges = []
+    with open(fname, "r") as fin:
+        n, m = map(int, fin.readline().split())
+        pos = {}
+        for _ in range(n):
+            v, x, y = fin.readline().split()
+            pos[int(v)] = float(x), float(y)
+        minx = 10**10
+        miny = 10**10
+        for v, (x, y) in pos.items():
+            minx = min(x, minx)
+            miny = min(y, miny)
+        for v in pos:
+            x, y = pos[v]
+            pos[v] = round((x - minx) / 10000, 2), round((y - miny) / 10000, 2)
+        for _ in range(m):
+            u, v = map(int, fin.readline().split())
+            edges.append((u, v, dist(u, v, pos)))
+    G = nx.Graph()
+    for u, v, w in edges:
+        G.add_edge(u, v, weight=w)
+    return G, pos
+
+
+G, pos = read_graph(sys.argv[1])
+K = 2
+s = 0
+t = 27  # len(G.nodes)-1
+d = cheapest_walk(G, s, K)
+prev_v = -1
+for (k, p, v), ell in sorted(d.items(), key=lambda e: (e[0][2], e[0][0], e[0][1])):
+    if v != prev_v:
+        print("\n\t ====", v, "====")
+        print("\t k p v   ell")
+        print("\t -----------")
+        prev_v = v
+    print("\t", k, p, v, ell)
+    if p == 1 and k == K:
+        print("    ==>     ", v, ell)
+
+plot_graph(G, [s, t], pos)
